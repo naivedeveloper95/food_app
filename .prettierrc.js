abd8b30e36186912f0ad07f13fa8318244773754
naivedeveloper95 <findsatyam95@gmail.com>
2021-07-30 11:19:36 +0530

    Changes

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,6 @@
 module.exports = {
-  bracketSpacing: true,
-  jsxBracketSameLine: true,
-  singleQuote: true,
-  trailingComma: 'none'
+	bracketSpacing: true,
+	jsxBracketSameLine: true,
+	singleQuote: true,
+	trailingComma: 'none'
 };
--- screens/CardListScreen.js
+++ screens/CardListScreen.js
@@ -4,34 +4,34 @@ import { data } from '../model/data';
 import Card from '../components/Card';
 
 const CardListScreen = ({ navigation }) => {
-  const renderItem = ({ item }) => {
-    return (
-      <Card
-        itemData={item}
-        onPress={() =>
-          navigation.navigate('CardItemDetails', { itemData: item })
-        }
-      />
-    );
-  };
+	const renderItem = ({ item }) => {
+		return (
+			<Card
+				itemData={item}
+				onPress={() =>
+					navigation.navigate('CardItemDetails', { itemData: item })
+				}
+			/>
+		);
+	};
 
-  return (
-    <View style={styles.container}>
-      <FlatList
-        data={data}
-        renderItem={renderItem}
-        keyExtractor={(item) => item.id}
-      />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<FlatList
+				data={data}
+				renderItem={renderItem}
+				keyExtractor={(item) => item.id}
+			/>
+		</View>
+	);
 };
 
 export default CardListScreen;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    width: '90%',
-    alignSelf: 'center'
-  }
+	container: {
+		flex: 1,
+		width: '90%',
+		alignSelf: 'center'
+	}
 });
